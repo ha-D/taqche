9f50f1e2bbb8426d962c03c644067db154cfebb5
Hadi Zolfaghari <hadi.zolfaghaari@gmail.com>
2020-03-06 14:13:58 +0330

    Remove backend and include elastic settings

--- backend/taqche/settings.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-
-TIMEZONE = getenv('TIMEZONE', 'Asia/Tehran')
-
-DATABASE = {
-    'host': getenv('DB_HOST', 'localhost'),
-    'port': int(getenv('DB_PORT', '27017')),
-    'name': getenv('DB_NAME', 'taqche')
-}
